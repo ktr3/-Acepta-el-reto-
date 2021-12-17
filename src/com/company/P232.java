429d8a71e06ac7cb7b05118aef67b18ddb8c41f1
kevin <kevin.yaguana1@gmail.com>
2021-12-17 23:05:58 +0100

    Empezado Ejercicio 232

---
+++ src/com/company/P232.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class P232 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		
+		int diaCumple, mesCumple, anyoCumple;
+		int diaActual, mesActual, anyoActual;
+		
+		int meses[] = new int [] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		
+		int diasNoCumple, diasCumple, diasActual;
+		
+		while(true) { 
+			
+			diaCumple = in.nextInt();
+			mesCumple = in.nextInt();
+			anyoCumple = in.nextInt();
+			diaActual = in.nextInt();
+			mesActual = in.nextInt();
+			anyoActual = in.nextInt();
+			
+			
+			
+		}
+	}
+	
+	
+	
+	
+}
